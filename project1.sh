def933e531f1c856474668a70ba7925f2d0b8cc3
Cloud User <ec2-user@ip-172-31-13-60.us-east-2.compute.internal>
2020-01-26 12:46:15 +0000

    commiting files

---
+++ project1.sh
@@ -0,0 +1,6 @@
+git is a command
+we can edit modify commit files in git
+in git workspace staging ares git repo will present
+sto move from workspace to staging area we use git add
+from staging area to local repo we use  git commit -m "comment"
+
